904f135b98761e4c0ac50c9efaf033b6b197a681
mhatvan <markus_hatvan@aon.at>
2018-01-25 23:19:30 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  // save stuff to local storage
+
+  // public newTodo: {
+  //   title: 'asdf',
+  //   description: 'asdf',
+  //   urgent: null,
+  //   done: null
+  // };
+
+  public todosFiltered: any;
+
+  public todos: Array<any> = [
+    {
+      title: 'clean katzenkisterl',
+      description: 'do it every second day, bag is outside the window',
+      urgent: true,
+      done: false
+    },
+    {
+      title: 'hang up clothes',
+      description: 'take clothes out of washing machine, hang them up in the bedroom',
+      urgent: false,
+      done: false
+    }
+  ];
+
+  public ngOnInit() {
+    this.todosFiltered = this.todos;
+  }
+
+  public searchForTodo(event) {
+    if (event.target.value === '' || event.target.value !== '' && event.keyCode === 13) {
+      this.todosFiltered = this.todos.filter(elem => {
+        return (elem.title).includes(event.target.value) || (elem.description).includes(event.target.value);
+      });
+    }
+  }
+
+  public onSubmit() {
+    console.log('triggered');
+    // if (this.myform.valid) {
+    //   console.log("Form Submitted!");
+    // }
+  }
 }
